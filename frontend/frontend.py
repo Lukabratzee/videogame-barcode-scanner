2deca3124d14e4d1c70830c0cabf6f21cf733e1a
lukabratzee <luke.clayton@btopenworld.com>
2024-08-06 14:59:53 +0100

    Added coment to run 'localhost' if using Python

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -3,8 +3,8 @@ import requests
 import time
 import os
 
-# Retrieve the backend host from environment variables, default to 'localhost' if not set
-backend_host = os.getenv('BACKEND_HOST', 'backend')
+# Retrieve the backend host from environment variables, default to 'localhost' if using Python locally
+backend_host = os.getenv('BACKEND_HOST', 'localhost') 
 backend_port = 5001  # Assuming the backend is running on this port
 
 # Backend API base URL
