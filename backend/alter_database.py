1bdf13b9da7dab8650443e46a2f11f2287cf6e6e
lukabratzee <luke.clayton@btopenworld.com>
2024-08-08 22:12:15 +0100

    Need to fix IGDB enter. Can retrive without year or cover art, crashes DB

--- backend/alter_database.py
+++ backend/alter_database.py
@@ -3,8 +3,8 @@ import sqlite3
 conn = sqlite3.connect('games.db')
 cursor = conn.cursor()
 
-# Add the average_price column to the games table
-cursor.execute('ALTER TABLE games ADD COLUMN average_price REAL')
+# Drop the column 'series'
+cursor.execute('ALTER TABLE games DROP COLUMN series')
 
 conn.commit()
 conn.close()
