0270fbc8c5d8d3eb34658b73caa6397258968256
lukabratzee <luke.clayton@btopenworld.com>
2025-05-08 20:02:23 +0100

    Added platforms list to implement

--- modules/scrapers.py
+++ modules/scrapers.py
@@ -98,26 +98,6 @@ def scrape_amazon_price(game_title):
     finally:
         driver.quit()
 
-import re
-import time
-import logging
-import undetected_chromedriver as uc
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service as ChromeService
-
-import re
-import time
-import logging
-import undetected_chromedriver as uc
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service as ChromeService
-
 def scrape_ebay_prices(game_title):
     """
     Opens eBay UK's homepage, enters the game_title in the search box,
